10cf31e7bf932dda99b983778e154fedf15f63b9
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-09-12 13:57:52 +0200

    ♻️ refactor funcs into triangle moduule

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,19 @@
 import p5 from "p5"
 import { hexToHSL } from "./utils"
-import Delaunator from "delaunator"
+
 import "./styles.scss"
+import {
+  Triangle,
+  generateInitialStateDelaunay,
+  subdivideTriangle,
+} from "./triangle"
 
 const padding = 50
 const width = 800 + padding
 const height = width * Math.sqrt(2) + padding
 
-const drawStrokes = false
+const drawStrokes = true
 const drawFaces = true
-
-interface Point {
-  x: number
-  y: number
-}
-
-interface Triangle {
-  A: Point
-  B: Point
-  C: Point
-}
 
 const colorPalette = [
   "#E23E57",
@@ -48,8 +42,13 @@ function generateInitialTriangles(): Triangle[] {
 const sketch = (p: p5) => {
   let nIter = 0
   // let triangles = generateInitialTriangles()
-  const nInitialPoints = 3
-  let triangles = generateInitialStateDelaunay(nInitialPoints)
+  const nInitialPoints = 1
+  let triangles = generateInitialStateDelaunay(
+    nInitialPoints,
+    width,
+    height,
+    padding
+  )
 
   p.setup = () => {
     const canvas = p.createCanvas(width + 10, height + 10)
@@ -58,7 +57,7 @@ const sketch = (p: p5) => {
     p.colorMode(p.HSL)
     p.noFill()
 
-    drawStrokes ? p.strokeWeight(0.1) : p.strokeWeight(0)
+    drawStrokes ? p.strokeWeight(0.05) : p.strokeWeight(0)
     p.frameRate(30)
   }
 
@@ -91,133 +90,10 @@ const sketch = (p: p5) => {
     triangles = newTriangles
     nIter += 1
 
-    if (nIter > 12) {
+    if (nIter > 11) {
       p.noLoop()
     }
   }
 }
 
-function subdivideTriangle(triangle: Triangle): Triangle[] {
-  const [P1, P2] = getLongestSidePoints(triangle)
-  const P3 = Object.keys(triangle).filter((key) => key !== P1 && key !== P2)[0]
-  const D = getMidpoint(triangle[P1], triangle[P2])
-
-  return [
-    { A: triangle[P1], B: triangle[P3], C: D } as Triangle,
-    { A: triangle[P2], B: triangle[P3], C: D },
-  ]
-}
-
-function getMidpoint(A: Point, B: Point): Point {
-  // const rand = 0
-  const rand = (Math.random() - 0.5) * 22
-  return { x: (A.x + B.x) / 2 + rand, y: (A.y + B.y) / 2 + rand }
-}
-
-function getLongestSidePoints(triangle): string[] {
-  const lengths = triangleSideLengths(triangle)
-  const values = Object.values(lengths)
-  const max = Math.max.apply(Math, values)
-  return Object.keys(lengths)
-    .reduce((a, b) => (lengths[a] > lengths[b] ? a : b))
-    .split("")
-}
-
-function triangleSideLengths(triangle: Triangle): {
-  AB: number
-  BC: number
-  AC: number
-} {
-  return {
-    AB: getLength(triangle.A, triangle.B),
-    BC: getLength(triangle.B, triangle.C),
-    AC: getLength(triangle.A, triangle.C),
-  }
-}
-
-function getLength(A: Point, B: Point): number {
-  return Math.sqrt((A.x - B.x) ** 2 + (A.y - B.y) ** 2)
-}
-
 new p5(sketch)
-
-function loopSubdivision(triangle: Triangle): Triangle[] {
-  const resultingTriangles = []
-
-  const AB = getMidpoint(triangle.A, triangle.B)
-  const BC = getMidpoint(triangle.B, triangle.C)
-  const AC = getMidpoint(triangle.A, triangle.C)
-
-  resultingTriangles.push({
-    A: triangle.A,
-    B: AB,
-    C: AC,
-  })
-  resultingTriangles.push({
-    A: AB,
-    B: triangle.B,
-    C: BC,
-  })
-  resultingTriangles.push({
-    A: AC,
-    B: BC,
-    C: triangle.C,
-  })
-  resultingTriangles.push({
-    A: AB,
-    B: BC,
-    C: AC,
-  })
-
-  return resultingTriangles
-}
-
-function getDistance(A: Point, B: Point) {
-  return Math.sqrt((A.x - B.x) ** 2 + (A.y - B.y) ** 2)
-}
-
-function generateInitialStateDelaunay(nPoints: number): Triangle[] {
-  const points: number[][] = [
-    [0 + padding, 0 + padding],
-    [0 + padding, height - padding],
-    [width - padding, 0 + padding],
-    [width - padding, height - padding],
-  ]
-  for (let n = 0; n < nPoints; n++) {
-    const x = Math.random() * (width - padding) + padding
-    const y = Math.random() * (height - padding) + padding
-
-    let tooClose = false
-    points.forEach((p) => {
-      if (Math.sqrt((x - p[0]) ** 2 + (y - p[1]) ** 2) < 150) {
-        tooClose = true
-      }
-    })
-
-    if (tooClose) {
-      n -= 1
-    } else {
-      points.push([x, y])
-    }
-  }
-
-  let delaunay = Delaunator.from(points)
-  const triangles: Triangle[] = []
-  for (let i = 0; i < delaunay.triangles.length; i += 3) {
-    triangles.push({
-      A: {
-        x: points[delaunay.triangles[i]][0],
-        y: points[delaunay.triangles[i]][1],
-      },
-      B: {
-        x: points[delaunay.triangles[i + 1]][0],
-        y: points[delaunay.triangles[i + 1]][1],
-      },
-      C: {
-        x: points[delaunay.triangles[i + 2]][0],
-        y: points[delaunay.triangles[i + 2]][1],
-      },
-    })
-  }
-  return triangles
-}
---
+++ src/triangle/index.ts
@@ -0,0 +1,140 @@
+import Delaunator from "delaunator"
+
+export interface Point {
+  x: number
+  y: number
+}
+
+export interface Triangle {
+  A: Point
+  B: Point
+  C: Point
+}
+
+export function subdivideTriangle(triangle: Triangle): Triangle[] {
+  const [P1, P2] = getLongestSidePoints(triangle)
+  const P3 = Object.keys(triangle).filter((key) => key !== P1 && key !== P2)[0]
+  const D = getMidpoint(triangle[P1], triangle[P2])
+
+  return [
+    { A: triangle[P1], B: triangle[P3], C: D } as Triangle,
+    { A: triangle[P2], B: triangle[P3], C: D },
+  ]
+}
+
+function getMidpoint(A: Point, B: Point): Point {
+  // const rand = 0
+  const rand = (Math.random() - 0.5) * 22
+  return { x: (A.x + B.x) / 2 + rand, y: (A.y + B.y) / 2 + rand }
+}
+
+function getLongestSidePoints(triangle): string[] {
+  const lengths = triangleSideLengths(triangle)
+  const values = Object.values(lengths)
+  const max = Math.max.apply(Math, values)
+  return Object.keys(lengths)
+    .reduce((a, b) => (lengths[a] > lengths[b] ? a : b))
+    .split("")
+}
+
+function triangleSideLengths(triangle: Triangle): {
+  AB: number
+  BC: number
+  AC: number
+} {
+  return {
+    AB: getLength(triangle.A, triangle.B),
+    BC: getLength(triangle.B, triangle.C),
+    AC: getLength(triangle.A, triangle.C),
+  }
+}
+
+function getLength(A: Point, B: Point): number {
+  return Math.sqrt((A.x - B.x) ** 2 + (A.y - B.y) ** 2)
+}
+
+function loopSubdivision(triangle: Triangle): Triangle[] {
+  const resultingTriangles = []
+
+  const AB = getMidpoint(triangle.A, triangle.B)
+  const BC = getMidpoint(triangle.B, triangle.C)
+  const AC = getMidpoint(triangle.A, triangle.C)
+
+  resultingTriangles.push({
+    A: triangle.A,
+    B: AB,
+    C: AC,
+  })
+  resultingTriangles.push({
+    A: AB,
+    B: triangle.B,
+    C: BC,
+  })
+  resultingTriangles.push({
+    A: AC,
+    B: BC,
+    C: triangle.C,
+  })
+  resultingTriangles.push({
+    A: AB,
+    B: BC,
+    C: AC,
+  })
+
+  return resultingTriangles
+}
+
+function getDistance(A: Point, B: Point) {
+  return Math.sqrt((A.x - B.x) ** 2 + (A.y - B.y) ** 2)
+}
+
+export function generateInitialStateDelaunay(
+  nPoints: number,
+  width: number,
+  height: number,
+  padding: number
+): Triangle[] {
+  const points: number[][] = [
+    [0 + padding, 0 + padding],
+    [0 + padding, height - padding],
+    [width - padding, 0 + padding],
+    [width - padding, height - padding],
+  ]
+  for (let n = 0; n < nPoints; n++) {
+    const x = Math.random() * (width - padding) + padding
+    const y = Math.random() * (height - padding) + padding
+
+    let tooClose = false
+    points.forEach((p) => {
+      if (Math.sqrt((x - p[0]) ** 2 + (y - p[1]) ** 2) < 150) {
+        tooClose = true
+      }
+    })
+
+    if (tooClose) {
+      n -= 1
+    } else {
+      points.push([x, y])
+    }
+  }
+
+  let delaunay = Delaunator.from(points)
+  const triangles: Triangle[] = []
+  for (let i = 0; i < delaunay.triangles.length; i += 3) {
+    triangles.push({
+      A: {
+        x: points[delaunay.triangles[i]][0],
+        y: points[delaunay.triangles[i]][1],
+      },
+      B: {
+        x: points[delaunay.triangles[i + 1]][0],
+        y: points[delaunay.triangles[i + 1]][1],
+      },
+      C: {
+        x: points[delaunay.triangles[i + 2]][0],
+        y: points[delaunay.triangles[i + 2]][1],
+      },
+    })
+  }
+  return triangles
+}
