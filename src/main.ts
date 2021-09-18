bc415c45241fe95a8e7cd29934e5d774f1fcfd10
Alex <17277800+alex-schaaf@users.noreply.github.com>
2021-09-18 08:26:47 +0200

    ➕ add seedrandom

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import p5 from "p5"
 import { hexToHSL } from "./utils"
+import seedrandom from "seedrandom"
 
 import "./styles.scss"
 import {
@@ -8,20 +9,69 @@ import {
   subdivideTriangle,
 } from "./triangle"
 
-const width = 2480 + 2480 * 0.0625
+const width = 600 + 600 * 0.0625
 const padding = width * 0.0625
 const height = width * Math.sqrt(2) + padding
 
 const drawStrokes = true
 const drawFaces = true
-const nInitialPoints = 1
+const nInitialPoints = 2
 
 const pinks = ["#E23E57", "#88304E", "#522546", "#311D3F"]
 const blues = ["#071330", "#0C4160", "#738FA7", "#C3CEDA"]
-
-const colorPalette = [...pinks, ...blues].map((hex) => hexToHSL(hex))
+const greenYellows = [
+  "#99e2b4",
+  "#88d4ab",
+  "#78c6a3",
+  "#67b99a",
+  "#56ab91",
+  "#469d89",
+  "#358f80",
+  "#248277",
+  "#14746f",
+  "#036666",
+]
+const reds = [
+  "#641220",
+  "#6e1423",
+  "#85182a",
+  "#a11d33",
+  "#a71e34",
+  "#b21e35",
+  "#bd1f36",
+  "#c71f37",
+  "#da1e37",
+  "#e01e37",
+]
+const tealpurple = [
+  "#006466",
+  "#065a60",
+  "#0b525b",
+  "#144552",
+  "#1b3a4b",
+  "#212f45",
+  "#272640",
+  "#312244",
+  "#3e1f47",
+  "#4d194d",
+]
+const nature = [
+  "#582f0e",
+  "#7f4f24",
+  "#936639",
+  "#a68a64",
+  "#b6ad90",
+  "#c2c5aa",
+  "#a4ac86",
+  "#656d4a",
+  "#414833",
+  "#333d29",
+]
+const yellows = ["#ff4800", "#ff5400", "#ff6000", "#ff6d00", "#ff7900"]
+const colorPalette = [...yellows, ...blues].map((hex) => hexToHSL(hex))
 
 const seed: number | null = 1
+seedrandom(seed, { global: true })
 
 function generateInitialTriangles(): Triangle[] {
   let triangles: Triangle[] = []
@@ -50,6 +100,8 @@ const sketch = (p: p5) => {
   )
 
   p.setup = () => {
+    p.randomSeed(seed)
+    p.noiseSeed(seed)
     const canvas = p.createCanvas(width + 10, height + 10)
     canvas.parent("p5-canvas")
     p.background(255, 255, 255)
